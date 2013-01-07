3e9850816671567284ae9cfe508cdb605436ba93
team4814 <wefirst@uwo.ca>
2013-01-07 02:28:02 -0500

    First commit from FRC laptop

--- Ultimate Ascent/src/com/wefirst/ultimateascent/UltimateAscent.java
+++ Ultimate Ascent/src/com/wefirst/ultimateascent/UltimateAscent.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
+// this is a change!
 package com.wefirst.ultimateascent;
 
 
