3b72913e206c6e190b32cc94105dd76ff083b8f3
matty961 <matty961@gmail.com>
2013-01-12 13:39:55 -0500

    added camera functionality (hopefully) - not sure yet how box detection is going to be implemented

--- Ultimate Ascent/src/com/wefirst/ultimateascent/UltimateAscent.java
+++ Ultimate Ascent/src/com/wefirst/ultimateascent/UltimateAscent.java
@@ -10,7 +10,16 @@ package com.wefirst.ultimateascent;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.camera.AxisCameraException;
+import edu.wpi.first.wpilibj.image.BinaryImage;
+import edu.wpi.first.wpilibj.image.ColorImage;
+import edu.wpi.first.wpilibj.image.CriteriaCollection;
+import edu.wpi.first.wpilibj.image.NIVision;
+import edu.wpi.first.wpilibj.image.NIVisionException;
+import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,18 +30,35 @@ import edu.wpi.first.wpilibj.Victor;
  */
 public class UltimateAscent extends SimpleRobot {
 
-    Victor motors[] = {new Victor(cRIOPorts.LEFT_FRONT_MOTOR), new Victor(cRIOPorts.LEFT_BACK_MOTOR), new Victor(cRIOPorts.RIGHT_FRONT_MOTOR), new Victor(cRIOPorts.RIGHT_BACK_MOTOR)};
+    Victor driveMotors[] = {new Victor(cRIOPorts.LEFT_MOTOR), new Victor(cRIOPorts.LEFT_MOTOR)};
     RobotDrive driveTrain;
     Joystick joystickLeft = new Joystick(cRIOPorts.LEFT_JOYSTICK);
     Joystick joystickRight = new Joystick(cRIOPorts.RIGHT_JOYSTICK);
     Joystick joystickShoot = new Joystick(cRIOPorts.SHOOTING_JOYSTICK);
+    AxisCamera cam;
+    CriteriaCollection cc;
+
+    public void robotMain() { // for testing
+        robotInit();
+        operatorControl();
+    }
 
     /**
      * This function is called once at execution
      */
     protected void robotInit() {
         try {
-            driveTrain = new RobotDrive(motors[0], motors[2]);
+            driveTrain = new RobotDrive(driveMotors[0], driveMotors[2]);
+            cam = AxisCamera.getInstance();
+            cam.writeMaxFPS(15);
+            cam.writeCompression(20);
+            cam.writeColorLevel(50);
+            cam.writeBrightness(40);
+            cam.writeResolution(AxisCamera.ResolutionT.k160x120);
+            // cam.writeExposureControl(AxisCamera.ExposureT.automatic);
+            cc = new CriteriaCollection();
+            cc.addCriteria(NIVision.MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 30, 400, false);
+            cc.addCriteria(NIVision.MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 40, 400, false);
         } catch (Exception any) {
             any.printStackTrace();
         }
@@ -44,6 +70,9 @@ public class UltimateAscent extends SimpleRobot {
     public void autonomous() {
         System.err.println("Entering autonomous:");
         driveTrain.setSafetyEnabled(false); // if true would stop the motors if there is no input, which there wouldn't be in autonomous
+        while (isAutonomous() && isEnabled()) {
+            imageGrab();
+        }
     }
 
     /**
@@ -54,12 +83,46 @@ public class UltimateAscent extends SimpleRobot {
         driveTrain.setSafetyEnabled(true); // stops the motors if input stops
         while (isOperatorControl() && isEnabled()) {
             try {
-                driveTrain.tankDrive(joystickLeft, joystickRight); // tank drive with "front" motors
-                motors[1].set(motors[0].get()); // set back "back" motors to same speed as "front"
-                motors[3].set(motors[2].get());
+                drive();
             } catch (Exception any) {
                 any.printStackTrace();
             }
+            for (int x = 0; x < driveMotors.length; x++) {
+                driveMotors[x].set(0f);
+            }
         }
+    }
+
+    public void imageGrab() {
+        ColorImage image;
+        try {
+            image = cam.getImage();
+            BinaryImage thresholdImage = image.thresholdRGB(0, 45, 0, 45, 0, 45);   // keep only white objects
+            BinaryImage bigObjectsImage = thresholdImage.removeSmallObjects(false, 2);  // remove small artifacts
+            BinaryImage convexHullImage = bigObjectsImage.convexHull(false); // fill in occluded rectangles
+            BinaryImage filteredImage = convexHullImage.particleFilter(cc); // find filled in rectangles
+
+            ParticleAnalysisReport[] reports = filteredImage.getOrderedParticleAnalysisReports();  // get list of results
+            for (int i = 0; i < reports.length; i++) {                                // print results
+                ParticleAnalysisReport r = reports[i];
+                System.out.println("Particle: " + i + ":  Center of mass x: " + r.center_mass_x);
+            }
+            System.out.println(filteredImage.getNumberParticles() + "  " + Timer.getFPGATimestamp());
+
+            filteredImage.free();
+            convexHullImage.free();
+            bigObjectsImage.free();
+            thresholdImage.free();
+            image.free();
+        } catch (AxisCameraException ex) {
+            ex.printStackTrace();
+        } catch (NIVisionException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void drive() {
+        driveTrain.tankDrive(joystickLeft, joystickRight); // tank drive
+        Timer.delay(0.1);
     }
 }
